47081dce4689c88c9788d31fc1a61a926672fc03
brendan wayne <brendan.t29@gmail.com>
2016-03-03 16:47:54 -0600

    various adjustments

--- sketch_multi_cell_A/sketch_multi_cell_A.ino
+++ sketch_multi_cell_A/sketch_multi_cell_A.ino
@@ -1,16 +1,20 @@
 #include <HX711.h>
 
-int pinsDOUT[4] = [1,3,5,7]; 
+int pinsDOUT[4] = {1,3,5,7}; 
 //The pins hooked up to the respective cells' DOUT
 
-int pinsSCK[4] = [2,4,6,8];
+int pinsSCK[4] = {2,4,6,8};
 //The pins hooked up to the respective cells' SCK
 
-HX711 allCells[4];
+float calibrations[4] = {-10000, -10000, -10000, -10000};
+//The initial calibration factors for the cells
+//HX711 fnord = scale(1,2);
+//HX711 allCells[] = {scale1(1,2), scale2(3,4), scale3(5,6), scale4(7,8)};
+HX711 allCells[4]; 
 
 void setup() {
-  for(ii=0, ii<4, ii++){
-    allCells[ii] = scale(pinsDOUT[ii], pinsSCK[ii]);
+  for(int ii=0; ii<4; ii++){
+    allCells[ii] = HX711(pinsDOUT[ii], pinsSCK[ii]);
     
   }
 }
